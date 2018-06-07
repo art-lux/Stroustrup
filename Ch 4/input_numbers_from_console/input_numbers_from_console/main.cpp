18b19755b798ee6d53b6e47ce765c61b02147c82
art lux <anlarionov@ANLARIONOV>
2018-06-07 16:00:34 +0300

    Task 7, 8

--- Ch 4/input_numbers_from_console/input_numbers_from_console/main.cpp
+++ Ch 4/input_numbers_from_console/input_numbers_from_console/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool isCorrectUnit(string& str)
+{
+	return str == "cm" || str == "in" || str == "ft" || str == "m";
+}
+
 void main()
 {
-	double input, max_value(numeric_limits<double>::min()), min_value(numeric_limits<double>::max());
-	while (cin >> input)
+	double input, max_value(numeric_limits<double>::min()), min_value(numeric_limits<double>::max()), sum = 0;
+	int input_counter = 0;
+	string unit;
+	while (cin >> input >> unit)
 	{
+		if (!isCorrectUnit(unit))
+		{
+			continue;
+		}
+		++input_counter;
 		if (input > max_value)
 		{
 			cout << "the largest value is: " << input << endl;
@@ -19,4 +32,7 @@ void main()
 		}
 		
 	}
+	cout << "the largest value was: " << max_value << endl;
+	cout << "the smallest value was: " << min_value << endl;
+	cout << "quantity of correct input was: " << input_counter << endl;
 }
