9310f77e45a8caf275efcb9c1ce8a84db8116265
art lux <anlarionov@ANLARIONOV>
2018-06-08 12:01:12 +0300

    Task 9

--- Ch 4/input_numbers_from_console/input_numbers_from_console/main.cpp
+++ Ch 4/input_numbers_from_console/input_numbers_from_console/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+constexpr double cm_coeff = 0.01, in_coef = 2.54 * cm_coeff, ft_coef = 12 * in_coef;
+
 bool isCorrectUnit(string& str)
 {
 	return str == "cm" || str == "in" || str == "ft" || str == "m";
@@ -19,7 +21,24 @@ void main()
 		{
 			continue;
 		}
+
 		++input_counter;
+
+		if ("cm" == unit)
+		{
+			input *= cm_coeff;
+		}
+		if ("in" == unit)
+		{
+			input *= in_coef;
+		}
+		if ("ft" == unit)
+		{
+			input *= ft_coef;
+		}
+
+		sum += input;
+
 		if (input > max_value)
 		{
 			cout << "the largest value is: " << input << endl;
@@ -35,4 +54,5 @@ void main()
 	cout << "the largest value was: " << max_value << endl;
 	cout << "the smallest value was: " << min_value << endl;
 	cout << "quantity of correct input was: " << input_counter << endl;
+	cout << "Sum: " << sum << endl;
 }
