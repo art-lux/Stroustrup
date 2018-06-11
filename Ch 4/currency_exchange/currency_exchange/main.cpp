47d92f16b84e4c8830c6d11e6dbc4c2f605e64ce
art lux <anlarionov@ANLARIONOV>
2018-06-11 12:02:38 +0300

    Exercise 1.2

--- Ch 4/currency_exchange/currency_exchange/main.cpp
+++ Ch 4/currency_exchange/currency_exchange/main.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
+
+char check_unit_sw(string& str)
+{
+	return str[0];
+}
 
 int main()
 {
 	constexpr double usd_coef = 26.04;
-	double quantity = 1;
+	double quantity = nan("");
 	string unit;
 	cout << "Please input the lengh"
 		<< " and unit (us or gr) : \n";
 	cin >> quantity >> unit;
-	if (unit == "us")
+	
+	switch (check_unit_sw(unit))
+	{
+	case 'u':
 		cout << quantity << " usd == "
-		<< usd_coef * quantity << " gr\n ";
-	else if (unit == "gr")
+			<< usd_coef * quantity << " gr\n ";
+	case 'g':
 		cout << quantity << " gr == "
-		<< quantity / usd_coef << " usd\n";
-	else
+			<< quantity / usd_coef << " usd\n";
+	default:
 		cout << "Sorry, I don't know what is it " << unit << "\n ";
+	}
+	
+	
+	
+	//if (unit == "us")
+	//	cout << quantity << " usd == "
+	//	<< usd_coef * quantity << " gr\n ";
+	//else if (unit == "gr")
+	//	cout << quantity << " gr == "
+	//	<< quantity / usd_coef << " usd\n";
+	//else
+	//	cout << "Sorry, I don't know what is it " << unit << "\n ";
 	return 0;
 }
