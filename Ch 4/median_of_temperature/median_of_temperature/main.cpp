8c692ed4519d55cbbd48a95a7b4b66b8a7876742
art lux <anlarionov@ANLARIONOV>
2018-06-11 14:33:31 +0300

    Exercise 2

---
+++ Ch 4/median_of_temperature/median_of_temperature/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	vector<double> temps;
+	for (double temp; cin >> temp;)
+	{
+		temps.push_back(temp);
+	}
+
+	sort(temps.begin(), temps.end());
+
+	if (temps.size() % 2 == 0)
+	{
+		cout << (temps[temps.size() / 2 - 1] + temps[temps.size() / 2]) / 2 << endl;
+	}
+	else
+	{
+		cout << temps[temps.size() / 2] << endl;
+	}
+
+	for (auto el : temps)
+	{
+		cout << el << endl;
+	}
+	return 0;
+}
