77d1a58a7552bf6ff23d4105f3fd71245c50dbd3
art lux <anlarionov@ANLARIONOV>
2018-06-11 12:30:04 +0300

    Exercise 1.4

--- Ch 4/ascii_table_output/ascii_table_output/main.cpp
+++ Ch 4/ascii_table_output/ascii_table_output/main.cpp
@@ -7,10 +7,19 @@ using namespace std;
 
 int main()
 {
-	for (unsigned char x = 0; x < 255; x++)
+	//for (unsigned char x = 0; x < 255; x++)
+	//{
+	//	cout << (int)x << ": " << x << endl;
+	//	this_thread::sleep_for(chrono::milliseconds(50) );
+	//}
+
+	unsigned char x = 0;
+	while (x != 255)
 	{
 		cout << (int)x << ": " << x << endl;
 		this_thread::sleep_for(chrono::milliseconds(50) );
+		++x;
 	}
+
 	return 0;
 }
