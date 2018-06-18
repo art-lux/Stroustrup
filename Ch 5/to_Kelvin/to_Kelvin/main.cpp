6efa390dfa869363c8e553e9eea121f186efcf79
art lux <anlarionov@ANLARIONOV>
2018-06-18 16:46:21 +0300

    Exercise 5

--- Ch 5/to_Kelvin/to_Kelvin/main.cpp
+++ Ch 5/to_Kelvin/to_Kelvin/main.cpp
@@ -13,10 +13,30 @@ double ctok(double c) // Преобразует темпера туру
 	return k;
 }
 
+double ktoc(double k)
+{
+	return k - 273.15;
+}
+
 int main()
 {
 	double c = 0;
-	cin >> c;
-	double k = ctok(c);
+	char t;
+	cout << "Enter temp and system('k' or 'c'): ";
+	cin >> c >> t;
+	double k;
+	switch (t)
+	{
+	case 'k':
+		k = ktoc(c);
+		break;
+	case 'c':
+		k = ctok(c);
+		break;
+	default:
+		cout << "Incorrect system!\n";
+		return 1;
+		break;
+	}
 	cout << k << endl;
 }
