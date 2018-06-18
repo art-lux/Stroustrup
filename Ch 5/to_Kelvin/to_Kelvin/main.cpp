510fb77a6fcfddad13064e65978ba78f9a4aeaad
art lux <anlarionov@ANLARIONOV>
2018-06-18 16:38:07 +0300

    Exercise 4

--- Ch 5/to_Kelvin/to_Kelvin/main.cpp
+++ Ch 5/to_Kelvin/to_Kelvin/main.cpp
@@ -2,20 +2,21 @@
 
 using namespace std;
 
-double ctok(double с) // Преобразует темпера туру
+double ctok(double c) // Преобразует темпера туру
 { // в градусах Цель сия в темпера туру в градуса х Кельвина
-	double k = с + 273.15;
+	if (c < -273.15)
+	{
+		cout << "incorrect input!\n" << endl;
+		exit(1);
+	}
+	double k = c + 273.15;
 	return k;
 }
 
 int main()
 {
 	double c = 0;
-	for (;;) {
-		cin >> c;
-		if (c > -273.15) break;
-		cout << "incorrect input!\n" << endl;
-	}
+	cin >> c;
 	double k = ctok(c);
 	cout << k << endl;
 }
